16976f37b7309e9ebc121b5dd868f4932c010c26
HARYANA-ALAA <ankit6@sheridancollege.ca>
2023-05-15 14:26:49 -0400

    studnet progrmam n0.1

---
+++ src/studentsdetials/StudentsDetials.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package studentsdetials;
+
+/**
+ *
+ * @author User
+ */
+public class StudentsDetials {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Students [] list = new list();
+        Students s1 = new Students();
+        s1.setname("Ankit");
+        s1.setage("20");
+        
+        Students s2 = new Students();
+        s2.setname("Aniket");
+        s2.setage("15");
+        
+        Students s3 = new Students();
+        s3.setname("Sachin");
+        s3.setage("19");
+        
+        list[0] = s1;
+        list[1] = s2;
+        list[2] = s3;
+        
+        for(int i=0;i<list.length;i++)
+            System.out.println(list[i].getname + " " + list[i].getage);
+        // TODO code application logic here
+    }
+    
+}
